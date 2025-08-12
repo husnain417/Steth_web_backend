c4d48f75803489ef736d2580dc89a167779e6258
ibbi <ibrahimimransadiq@gmail.com>
2025-08-12 16:06:23 +0500

    imagekit

--- src/config/imageKit.js
+++ src/config/imageKit.js
@@ -1,5 +1,6 @@
 const ImageKit = require('imagekit');
 
+// Configure ImageKit
 const imagekit = new ImageKit({
   publicKey: process.env.IMAGEKIT_PUBLIC_KEY,
   privateKey: process.env.IMAGEKIT_PRIVATE_KEY,
